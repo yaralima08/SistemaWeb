6ec5d90215028deb47264c398902cb66f17e472d
yaralima08 <yara.lima@estudante.ufscar.br>
2026-05-29 11:43:44 -0300

    Delete IRoupasDAO.java

--- IRoupasDAO.java
+++
@@ -1,9 +0,0 @@
-package br.ufscar.dc.dsw.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ufscar.dc.dsw.domain.Roupas;
-
-public interface IRoupasDAO extends JpaRepository<Roupas, Long> {
-}
-
