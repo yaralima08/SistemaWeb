4148b2d47b3ccf5fb31e2d266eef4e37585e9162
yaralima08 <yara.lima@estudante.ufscar.br>
2026-05-29 11:43:56 -0300

    Delete ILojaDAO.java

--- ILojaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.ufscar.dc.dsw.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ufscar.dc.dsw.domain.Loja;
-
-public interface ILojaDAO extends JpaRepository<Loja, Long> {
-}
-
