ad93190b2880f6c6e8a8de31d62b38b32798082d
yaralima08 <yara.lima@estudante.ufscar.br>
2026-05-29 15:58:43 +0000

    Estrutura projeto T5

---
+++ src/main/java/br/ufscar/dc/dsw/dao/IRoupasDAO.java
@@ -0,0 +1,8 @@
+package br.ufscar.dc.dsw.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufscar.dc.dsw.domain.Roupas;
+
+public interface IRoupasDAO extends JpaRepository<Roupas, Long> {
+}
